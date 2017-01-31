72b4b8db19a0df28f83d116f41cb040cd3327872
Kneelawk <jedidiah.pommert@gmail.com>
2017-01-31 04:04:54 -0800

    Remove old code

--- fractalGenerator.js
+++ fractalGenerator.js
@@ -38,29 +38,6 @@ class Fractal {
 
   start() {
     native_fractal_service.startGenerator(this.uuid);
-    // this.pixelsGenerated = 0;
-    //
-    // for (let y = 0; y < this.imageHeight; y++) {
-    //   for (let x = 0; x < this.imageWidth; x++) {
-    //     // let other fractals gen at the same time
-    //     setImmediate(() => {
-    //       let i = (x + y * this.imageWidth) * 4;
-    //
-    //       this.genPixel(x, y, (color) => {
-    //         this.png.data[i] = color.r;
-    //         this.png.data[i + 1] = color.g;
-    //         this.png.data[i + 2] = color.b;
-    //         this.png.data[i + 3] = color.a;
-    //
-    //         // node is single threaded
-    //         this.pixelsGenerated++;
-    //         if (this.done()) {
-    //           this.save();
-    //         }
-    //       });
-    //     });
-    //   }
-    // }
   }
 
   save(halted, buffer) {
@@ -79,35 +56,6 @@ class Fractal {
   pixelsGenerated() {
     return native_fractal_service.getProgress(this.uuid);
   }
-
-  // genPixel(x, y, callback) {
-  //   let fx = x * this.fractalWidth / this.imageWidth + this.fractalMinX;
-  //   let fy = y * this.fractalHeight / this.imageHeight + this.fractalMinY;
-  //
-  //   let z = math.complex(fx, fy);
-  //   let n = 0;
-  //
-  //   let calc = () => {
-  //     if (n >= this.iterations) {
-  //       callback({
-  //         r: 0,
-  //         g: 0,
-  //         b: 0,
-  //         a: 255
-  //       });
-  //     } else if (z.re * z.re + z.im + z.im > 16) {
-  //       let color = fromHSB(mod2(n * 3.3, 0.0, 256.0) / 256.0, 1.0, mod2(n * 16.0, 0.0, 256.0) / 256.0);
-  //       color.a = 255;
-  //       callback(color);
-  //     } else {
-  //       n++;
-  //       z = this.f(z, math.complex(fx, fy), this.constant);
-  //       setImmediate(calc);
-  //     }
-  //   };
-  //
-  //   setImmediate(calc);
-  // }
 }
 
 Fractal.done = function (fractal, pixelsGenerated) {
